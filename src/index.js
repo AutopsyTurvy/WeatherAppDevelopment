e79c7a34d19a0c84995703d64caa886b4093470c
AutopsyTurvy <102550358+AutopsyTurvy@users.noreply.github.com>
2022-07-30 16:49:08 +0200

    Looped through days in injected HTML to Javascript
    moved THML file to Javascript and injected it.
    We looped hardcoded data into our code-
    I learned a lot today!

--- src/index.js
+++ src/index.js
@@ -51,23 +51,27 @@ date.innerHTML = `${hours}:${minutes}, ${day}, ${dayNumber} ${month}--${year}`;
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
 
-  let forecastHTML = "Hello";
-
-  forecastHTML =
-    forecastHTML +
-    `<div id="forecast" class="weather-forecast">
-    <div class="row">
-    <div class="weather-forecast-date">Mon:</div>
-    <img src="http://openweathermap.org/img/wn/50d@2x.png" alt="" width="20px"/>
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tues"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-2">
+    <div class="weather-forecast-date">${day}</div>
+    <div> 🌧 </div>
 
     <div class="weather-forecast-temperatures">
         <span class="weather-forecast-temperature-max">18</span>
         <span class="weather-forecast-temperature-min">12</span>
-
-    </div>
-</div>
+        </div>
+        
+ <br />
+ <br />
 </div>`;
+  });
 
+  forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
 
